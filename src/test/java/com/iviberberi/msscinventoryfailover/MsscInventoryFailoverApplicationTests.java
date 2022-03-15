575878196301a8db73f5d3c22e79d2299738681f
ivi.berberi <23117123>
2022-03-15 17:38:24 +0100

    initial commit

---
+++ src/test/java/com/iviberberi/msscinventoryfailover/MsscInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iviberberi.msscinventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscInventoryFailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
